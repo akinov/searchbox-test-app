a23f2af86345d257ead1db9c8ea2cdd3447625d1
akinov <ayumoto10@gmail.com>
2020-05-06 13:56:52 +0900

    Edit Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ApplicationRecord
+  min_shingle_size = 1
+  min_gram = 1
   searchkick settings: {
     analysis: {
       filter: {
         searchkick_suggest_shingle: {
-          max_shingle_size: 4
+          max_shingle_size: min_shingle_size,
+          max_shingle_size: min_shingle_size + 3
         },
         searchkick_ngram: {
-          max_gram: 2
+          min_gram: min_gram,
+          max_gram: min_gram + 1
         }
       }
     }
