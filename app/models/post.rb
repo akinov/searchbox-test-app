eedd582635eb8c438102c048d6302c8b33d3075e
akinov <ayumoto10@gmail.com>
2020-05-06 14:05:27 +0900

    delete elasticsearch setting analysis.filter.searchkick_suggest_shingle.min_shingle_size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,10 @@
 class Post < ApplicationRecord
-  MIN_SHINGLE_SIZE = 1.freeze
   MIN_GRAM = 1.freeze
   searchkick settings: {
     analysis: {
       filter: {
         searchkick_suggest_shingle: {
-          min_shingle_size: MIN_SHINGLE_SIZE,
-          max_shingle_size: MIN_SHINGLE_SIZE + 3
+          max_shingle_size: 4
         },
         searchkick_ngram: {
           min_gram: MIN_GRAM,
